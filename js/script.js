84e566f4fcb31dda9a343149ac10921a836c7154
Basti576 <sebastianz.9811@gmail.com>
2022-09-15 16:46:29 +0200

    updated js

--- js/script.js
+++ js/script.js
@@ -30,16 +30,8 @@ window.addEventListener('load', ()=>{
 
 //Set Map Script
 function setMap(lat, lng, ip){
-<<<<<<< HEAD
-let map = L.map('map').setView([lat, lng], 13);
-console.log("---- MAP----");
-console.log(lat);
-console.log(lng);
-console.log(ip);
-=======
 map = L.map('map').setView([lat, lng], 13);
 marker = L.marker([lat, lng]);
->>>>>>> ea85dccd23b078c8b0ace437977d083bc8c0dd43
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -94,14 +86,7 @@ fetch(url)
     timezoneBox.innerHTML = data.location.timezone;
     lspBox.innerHTML = data.isp;
    
-<<<<<<< HEAD
-    //setMap(data.location.lat, data.location.lng, data.ip); 
-    updateMarker(data.location.lat, data.location.lng, data.ip);
-    
-
-=======
     updateMap(data.location.lat, data.location.lng, data.ip); 
->>>>>>> ea85dccd23b078c8b0ace437977d083bc8c0dd43
 })
 /*.catch(function(){
     console.error("ERROR WHILE FETCHING API");
@@ -114,14 +99,10 @@ fetch(url)
 
 
 function getInitialIp(){
-<<<<<<< HEAD
 
    
 
 let url= "https://geo.ipify.org/api/v2/country,city?apiKey=at_5bjtmEoKTG7ErbAWHLqcvEUfquebs&ipAddress";
-=======
-    let url= "https://geo.ipify.org/api/v2/country,city?apiKey=at_5bjtmEoKTG7ErbAWHLqcvEUfquebs&ipAddress";
->>>>>>> ea85dccd23b078c8b0ace437977d083bc8c0dd43
    
 fetch(url)
 .then((resp)=> resp.json())
@@ -140,15 +121,8 @@ fetch(url)
 
 
 
-<<<<<<< HEAD
-function updateMarker(lat, lng){
-    L.marker([lat, lng]).addTo(map)
-.bindPopup(ip)
-.openPopup();
-=======
 function updateMap(lat, lng, ip){
    marker.setLatLng([lat, lng]).update();
    marker.bindPopup("Your IP is: "+ip).openPopup();
    map.flyTo([lat, lng], 13);
->>>>>>> ea85dccd23b078c8b0ace437977d083bc8c0dd43
 }
